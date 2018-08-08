b62e9629713164251c0de00d6e4445ffae3e8230
chack1172 <NBerardozzi@gmail.com>
2018-08-08 15:23:59 +0200

    Fixed modal popup and error no permission

--- inc/languages/english/simplelikes.lang.php
+++ inc/languages/english/simplelikes.lang.php
@@ -13,13 +13,14 @@ $l['simplelikes_like_singular'] = 'like';
 $l['simplelikes_like_plural'] = 'likes';
 $l['simplelikes_and'] = 'and';
 $l['simplelikes_like_normal'] = '{1} {2} this post';
-$l['simplelikes_like_others'] = '{1} and <a href="#pid{4}" onclick="MyBB.popupWindow(\'/misc.php?action=post_likes&amp;post_id={4}\', null, false); return false;">{2} others {3}</a> this post';
+$l['simplelikes_like_others'] = '{1} and <a href="#pid{4}" onclick="MyBB.popupWindow(\'/misc.php?action=post_likes&amp;post_id={4}\', null, false); return false;">{2} others</a> {3} this post';
 
 $l['simplelikes_error_no_likes'] = 'Nobody has liked this post yet.';
 $l['simplelikes_error_post_id'] = 'No post ID provided';
 $l['simplelikes_error_user_id'] = 'No post ID provided. Are you sure you\'re accessing this function correctly?';
 $l['simplelikes_error_own_post'] = 'You are not able to like your own posts.';
 $l['simplelikes_error_perms'] = 'Your usergroup is not currently allowed to like posts.';
+$l['simplelikes_error_perms_view'] = 'You do not have permission to view who liked this post.';
 
 $l['simplelikes_thanks_title'] = 'Thanks!';
 $l['simplelikes_thanks'] = 'Thanks for liking this post. Taking you back to it now.';
