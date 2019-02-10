ab455a1bc70450311d393e4aef4f7b84cf9e321d
Nereo Berardozzi <NBerardozzi@gmail.com>
2019-02-10 16:35:50 +0100

    Tapatalk integration (#38)
    * Added support for tapatalk
    
    * Fixed indentation
    
    * Revert changes when uninstalling
    
    * Missing language
    
    * Show tapatalk integration only when plugin is active

--- inc/languages/english/admin/simplelikes.lang.php
+++ inc/languages/english/admin/simplelikes.lang.php
@@ -10,3 +10,11 @@ $l['simplelikes_importer_description'] = 'Description';
 $l['simplelikes_importer_actions'] = 'Actions';
 $l['simplelikes_import_success_count_imported'] = '{0} likes successfully imported!';
 $l['simplelikes_import_error'] = 'Error importing likes: {0}';
+
+$l['simplelikes_tapatalk_integration'] = '<b>Tapatalk integration:</b> ';
+$l['simplelikes_tapatalk_apply_changes'] = 'apply changes to tapatalk files.';
+$l['simplelikes_tapatalk_revert_changes'] = 'revert changes to tapatalk files.';
+$l['simplelikes_tapatalk_apply_success'] = 'Changes to tapatalk files applied successfully.';
+$l['simplelikes_tapatalk_apply_error'] = 'Couldn\'t apply changes to tapatalk files.';
+$l['simplelikes_tapatalk_revert_success'] = 'Changes to tapatalk files reverted successfully.';
+$l['simplelikes_tapatalk_revert_error'] = 'Couldn\'t revert changes to tapatalk files.';
